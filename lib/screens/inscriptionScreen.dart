ee21663ca8cbc59a1a13c910ae06d4fa67b68216
fatimahafid <fatima.hafid00@gmail.com>
2020-07-28 00:37:53 +0100

    frontendd

--- lib/screens/inscriptionScreen.dart
+++ lib/screens/inscriptionScreen.dart
@@ -31,139 +31,139 @@ class _InscriptionScreenState extends State<InscriptionScreen> {
           SafeArea(
             child: SingleChildScrollView(
 
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                SizedBox(height: SizeConfig.safeBlockHorizontal * 20),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: SizeConfig.safeBlockHorizontal * 5, vertical: SizeConfig.safeBlockHorizontal * 6),
-                  margin: EdgeInsets.only(left: SizeConfig.safeBlockHorizontal * 5, right: SizeConfig.safeBlockHorizontal * 5, bottom: SizeConfig.safeBlockHorizontal * 5),
-                  height: MediaQuery.of(context).size.height * 0.8,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(25),
-                        topRight: Radius.circular(25),
-                        bottomLeft: Radius.circular(25),
-                        bottomRight: Radius.circular(25),
-                      )),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  SizedBox(height: SizeConfig.safeBlockHorizontal * 20),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: SizeConfig.safeBlockHorizontal * 5, vertical: SizeConfig.safeBlockHorizontal * 6),
+                    margin: EdgeInsets.only(left: SizeConfig.safeBlockHorizontal * 5, right: SizeConfig.safeBlockHorizontal * 5, bottom: SizeConfig.safeBlockHorizontal * 5),
+                    height: MediaQuery.of(context).size.height * 0.8,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(25),
+                          topRight: Radius.circular(25),
+                          bottomLeft: Radius.circular(25),
+                          bottomRight: Radius.circular(25),
+                        )),
 
-                  child: Column(
+                    child: Column(
 
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.only(left: SizeConfig.safeBlockHorizontal * 5, right: SizeConfig.safeBlockHorizontal * 5, top: SizeConfig.safeBlockHorizontal * 0),
-                        child: Text("Inscription",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(left: SizeConfig.safeBlockHorizontal * 5, right: SizeConfig.safeBlockHorizontal * 5, top: SizeConfig.safeBlockHorizontal * 0),
+                          child: Text("Inscription",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Color(0xFFF032f41),
+                                fontSize: 30,
+                                fontFamily: "Pacificio",
+                              )),
+                        ),
+                        SizedBox(height: SizeConfig.safeBlockHorizontal * 4),
+                        Container(
+                          height: SizeConfig.safeBlockHorizontal * 15,
+                          child:CustomTextField(
+                            label: "Nom",
+                            icon: Icon(
+                              Icons.person,
+                              size: 27,
                               color: Color(0xFFF032f41),
-                              fontSize: 30,
-                              fontFamily: "Pacificio",
-                            )),
-                      ),
-                      SizedBox(height: SizeConfig.safeBlockHorizontal * 4),
-                      Container(
-                        height: SizeConfig.safeBlockHorizontal * 15,
-                      child:CustomTextField(
-                        label: "Nom",
-                        icon: Icon(
-                          Icons.person,
-                          size: 27,
-                          color: Color(0xFFF032f41),
-                        ),
-                      ),
-                      ),
-                      SizedBox(height: SizeConfig.safeBlockHorizontal * 4),
-                  Container(
-                    height: SizeConfig.safeBlockHorizontal * 15,
-                    child:
-                      CustomTextField(
-                        label: "Prenom",
-                        icon: Icon(
-                          Icons.person,
-                          size: 27,
-                          color: Color(0xFFF032f41),
-                        ),
-                      ),
-                  ),
-                      SizedBox(height: SizeConfig.safeBlockHorizontal * 2),
-                  Container(
-                    height: SizeConfig.safeBlockHorizontal * 15,
-                    child: CustomTextField(
-                        label: "CIN",
-                        isPassword: true,
-                        icon: Icon(
-                          Icons.credit_card,
-                          size: 27,
-                          color: Color(0xFFF032f41),
-                        ),
-                      ),
-                  ),
-                      SizedBox(height: SizeConfig.safeBlockHorizontal * 4),
-                  Container(
-                    height: SizeConfig.safeBlockHorizontal * 15,
-                    child:CustomTextField(
-                        label: "Téléphone",
-                        isPassword: true,
-                        icon: Icon(
-                          Icons.phone,
-                          size: 27,
-                          color: Color(0xFFF032f41),
-
-                        ),),
-                      ),
-                      SizedBox(height: SizeConfig.safeBlockHorizontal * 4),
-
-                      new Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-
-                          new Radio(
-                            value: 1,
-                            groupValue: group,
-                            onChanged: (T) {
-                              print(T);
-                            },
-                          ),
-                          new Text(
-                            'Femme',
-                            style: new TextStyle(
-                              fontSize: 16.0,
                             ),
                           ),
-                          new Radio(
-                            value: 2,
-                            groupValue: group,
-                            onChanged: (T) {
-                              print(T);
-                            },
+                        ),
+                        SizedBox(height: SizeConfig.safeBlockHorizontal * 4),
+                        Container(
+                          height: SizeConfig.safeBlockHorizontal * 15,
+                          child:
+                          CustomTextField(
+                            label: "Prenom",
+                            icon: Icon(
+                              Icons.person,
+                              size: 27,
+                              color: Color(0xFFF032f41),
+                            ),
                           ),
-                          new Text(
-                            'Homme',
-                            style: new TextStyle(fontSize: 16.0),
+                        ),
+                        SizedBox(height: SizeConfig.safeBlockHorizontal * 2),
+                        Container(
+                          height: SizeConfig.safeBlockHorizontal * 15,
+                          child: CustomTextField(
+                            label: "CIN",
+                            isPassword: true,
+                            icon: Icon(
+                              Icons.credit_card,
+                              size: 27,
+                              color: Color(0xFFF032f41),
+                            ),
                           ),
-                        ],
-                      ),
-                      SizedBox(height: SizeConfig.safeBlockHorizontal * 4),
-                  Container(
-                    height: SizeConfig.safeBlockHorizontal * 13,
-                    child:ButtonLoginAnimation(
-                        label: "S'inscrire",
-                        fontColor: Colors.white,
-                        background: Color(0xffe6b301),
-                        borderColor: Color(0xffe6b301),
-                        child: AjouterVehicule(),
-                      )
-                  ),
-                    ],
-                  ),
+                        ),
+                        SizedBox(height: SizeConfig.safeBlockHorizontal * 4),
+                        Container(
+                          height: SizeConfig.safeBlockHorizontal * 15,
+                          child:CustomTextField(
+                            label: "Téléphone",
+                            isPassword: true,
+                            icon: Icon(
+                              Icons.phone,
+                              size: 27,
+                              color: Color(0xFFF032f41),
 
-                )
-              ],
+                            ),),
+                        ),
+                        SizedBox(height: SizeConfig.safeBlockHorizontal * 4),
+
+                        new Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+
+                            new Radio(
+                              value: 1,
+                              groupValue: group,
+                              onChanged: (T) {
+                                print(T);
+                              },
+                            ),
+                            new Text(
+                              'Femme',
+                              style: new TextStyle(
+                                fontSize: 16.0,
+                              ),
+                            ),
+                            new Radio(
+                              value: 2,
+                              groupValue: group,
+                              onChanged: (T) {
+                                print(T);
+                              },
+                            ),
+                            new Text(
+                              'Homme',
+                              style: new TextStyle(fontSize: 16.0),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: SizeConfig.safeBlockHorizontal * 4),
+                        Container(
+                            height: SizeConfig.safeBlockHorizontal * 13,
+                            child:ButtonLoginAnimation(
+                              label: "S'inscrire",
+                              fontColor: Colors.white,
+                              background: Color(0xffe6b301),
+                              borderColor: Color(0xffe6b301),
+                              child: AjouterVehicule(),
+                            )
+                        ),
+                      ],
+                    ),
+
+                  )
+                ],
+              ),
             ),
-          ),
           ),
         ],
       ),
